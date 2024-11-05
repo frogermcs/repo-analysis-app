9d9370e73223dbd6d8bda86014bfc57aee12c2f7
Mirosław Stanek <froger.mcs@gmail.com>
2024-11-05 23:09:54 +0100

    Added example prompt page to display results from Google Gemini

--- src/pages/api/analysis-example.ts
+++ src/pages/api/analysis-example.ts
@@ -1,3 +1,4 @@
+import { prisma } from "@/lib/prisma";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import type { NextApiRequest, NextApiResponse } from 'next';
 
@@ -9,9 +10,20 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         }
         const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
         const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-        const prompt = "How are you?";
+
+        const prompt = await prisma.prompt.findUnique({
+            where: { id: "a093e184-5819-4887-b25d-6bfa00de5c86" },
+          }).then((prompt) => prompt?.text);
+        const repoPrompt = await prisma.repository.findUnique({
+            where: { id: "ae5ea965-db92-453e-8711-338bdb65744b" },
+          }).then((repo) => repo?.text);
         
-        const result = await model.generateContent([prompt]);
+        
+        if (!prompt || !repoPrompt) {
+            throw new Error("Prompt or Repository not found");
+        }
+
+        const result = await model.generateContent([prompt, repoPrompt]);
         console.log(result.response.text());
       res.status(200).json({ text: result.response.text() });
     } catch (error) {
---
+++ src/pages/repo/example-analysis.tsx
@@ -0,0 +1,22 @@
+
+import { useEffect, useState } from 'react';
+import useSWR from 'swr';
+import axios from 'axios';
+import Markdown from 'react-markdown'
+
+export default function RepoAnalysis() {
+    const [text, setText] = useState('');
+
+    const fetcher = (url: string) => axios.get(url).then((res) => res.data);
+    const { data, error } = useSWR(`/api/analysis-example`, fetcher);
+
+    useEffect(() => {
+        if (data) {
+          setText(data.text);
+        } else if (error) {
+          setText('Error fetching analysis: ' + error.message);
+        }
+      }, [data, error]);
+
+    return <div><Markdown>{text}</Markdown></div>
+}
