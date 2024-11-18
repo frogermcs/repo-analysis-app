3e08ece0ebf01fa541b347c06d37d8ade2f8d495
Mirosław Stanek <froger.mcs@gmail.com>
2024-11-18 19:24:49 +0100

    Simplified example markdown page routing

--- src/app/markdown/page.tsx
+++ src/app/markdown/page.tsx
@@ -20,15 +20,15 @@ export default function RepoAnalysis() {
     "End of example analysis.\n"
 
     return <SidebarInset>
-      <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-        <h1 className="text-lg font-bold">Analysis</h1>
-      </header>
-      <div className="flex flex-1 flex-col gap-4 p-4">
-      <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">        
-        <div className="flex flex-col space-y-1.5 p-6 prose">
-          <Markdown rehypePlugins={[rehypeHighlight]}>{exampleMarkdown}</Markdown>
-      </div>
-      </div>
-    </div>
-  </SidebarInset>
+            <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+              <h1 className="text-lg font-bold">Analysis</h1>
+            </header>
+            <div className="flex flex-1 flex-col gap-4 p-4">
+              <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">        
+                <div className="flex flex-col space-y-1.5 p-6 prose">
+                  <Markdown rehypePlugins={[rehypeHighlight]}>{exampleMarkdown}</Markdown>
+                </div>
+              </div>
+            </div>
+          </SidebarInset>
 }
