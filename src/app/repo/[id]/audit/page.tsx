4ae3854e19d65922f232c6b2c3e99de211de9bba
Mirosław Stanek <froger.mcs@gmail.com>
2024-11-18 19:25:00 +0100

    Added markdown formatting to example audit

--- src/app/repo/[id]/audit/page.tsx
+++ src/app/repo/[id]/audit/page.tsx
@@ -4,6 +4,10 @@ import { useEffect, useState } from 'react';
 import useSWR from 'swr';
 import axios from 'axios';
 import Markdown from 'react-markdown'
+import rehypeHighlight from 'rehype-highlight'
+import {
+  SidebarInset,
+} from "@/components/ui/sidebar"
 
 export default function RepoAnalysis() {
     const [text, setText] = useState('');
@@ -19,5 +23,16 @@ export default function RepoAnalysis() {
         }
       }, [data, error]);
 
-    return <div><Markdown>{text}</Markdown></div>
+      return <SidebarInset>
+      <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+        <h1 className="text-lg font-bold">Analysis</h1>
+      </header>
+      <div className="flex flex-1 flex-col gap-4 p-4">
+        <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">        
+          <div className="flex flex-col space-y-1.5 p-6 prose">
+            <Markdown rehypePlugins={[rehypeHighlight]}>{text}</Markdown>
+          </div>
+        </div>
+      </div>
+    </SidebarInset>
 }
